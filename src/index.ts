7c579c8835263e1a615e1c13ff122db7a62aba1e
Pedro Rosales <pedro.rosales@thejabb.com>
2025-04-25 13:25:43 -0400

    Agregando types en index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,43 @@
-import './global.d.ts';
+type Color =
+    | 'black'
+    | 'red'
+    | 'green'
+    | 'yellow'
+    | 'blue'
+    | 'magenta'
+    | 'cyan'
+    | 'white'
+    | 'brightBlack'
+    | 'brightRed'
+    | 'brightGreen'
+    | 'brightYellow'
+    | 'brightBlue'
+    | 'brightMagenta'
+    | 'brightCyan'
+    | 'brightWhite';
+
+type Format =
+    | 'reset'
+    | 'bold'
+    | 'faint'
+    | 'italic'
+    | 'underline'
+    | 'blinkingSlow'
+    | 'blinkingRapid'
+    | 'inverted'
+    | 'hidden'
+    | 'strikethrough';
+
+type Message =
+    | {
+          text: string | number | boolean;
+          color?: Color;
+          format?: Format;
+          background?: Color;
+      }[]
+    | string
+    | number
+    | boolean;
 
 enum color {
     black = 30,
